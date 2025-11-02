578fd939a9b958ad200e4635f16908600a3d7144
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-11-02 15:53:30 +0000

    myApp.js

--- myApp.js
+++ myApp.js
@@ -3,12 +3,12 @@ const express = require ('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI{
+mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser:true,
   useUnifiedTopology:true,
 });
 
-let Person;
+var Person;
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
