52d22827e358ac97904f01188226c8cb2a48d7eb
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-11-02 15:37:35 +0000

    .env y conecction

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
+const express = require ('express');
+const app = express();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+});
 
 let Person;
 
