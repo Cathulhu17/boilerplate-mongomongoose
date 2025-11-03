cd33d0b4c57bfcb13ed74a56ec300127d2401f2f
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-11-03 23:05:08 +0000

    createAndSavePerson

--- myApp.js
+++ myApp.js
@@ -84,13 +84,8 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
   done(null /*, data*/);
 };
-
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
